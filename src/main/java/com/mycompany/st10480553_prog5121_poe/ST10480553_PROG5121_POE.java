49bec614f74a3a44d8e8da912e9e99222c02342a
dulcy <dulcy@MapulaDulcy.lan>
2026-09-16 23:40:30 +0200

    Initial commit: starting the project Added username validation using while loop

--- src/main/java/com/mycompany/st10480553_prog5121_poe/ST10480553_PROG5121_POE.java
+++ src/main/java/com/mycompany/st10480553_prog5121_poe/ST10480553_PROG5121_POE.java
@@ -20,6 +20,7 @@ public class ST10480553_PROG5121_POE {
         String lastName;
         String userName;
         String phoneNum;
+        String password;
         
         //Ask user to enter name and surname
         System.out.print("Enter first name: ");
@@ -33,10 +34,23 @@ public class ST10480553_PROG5121_POE {
         userName = myInput.nextLine();
         
         while(!login.checkUserName(userName)){
-            System.out.println("Username is not correctly formatted please ensure that the ussername contains an underscore and is more than five characters in length.");
+            System.out.println("Username is not correctly formatted; please ensure that the ussername contains an underscore and is more than five characters in length.");
             System.out.print("Please re-enter username: ");
             userName = myInput.nextLine();
              }
+        System.out.println("Username successfully captured.");
         
+        //Loop the password to check if it's valid or not
+        System.out.print("Enter password (min 8 chars, 1 uppercase, 1 number, 1 special char):");
+        password = myInput.nextLine();
+        
+        while(!login.checkPassword(password)){
+            System.out.println("Password is not correctly formatted; please ensure that the password contains at least eight characters, a caoital letter, a number,and a special character.");
+            System.out.print("Please re-enter password: ");
+            password = myInput.nextLine();
+        }
+        System.out.println("Password successfully captured.");
+        
+        //
     }
 }
