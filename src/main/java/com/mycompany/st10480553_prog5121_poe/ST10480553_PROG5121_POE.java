7ec017b57a470ab53f6b194a76b8f8cf7c47fc2a
dulcy <dulcy@MapulaDulcy.lan>
2026-09-16 11:56:23 +0200

    Initial commit: starting the project

---
+++ src/main/java/com/mycompany/st10480553_prog5121_poe/ST10480553_PROG5121_POE.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.st10480553_prog5121_poe;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author dulcy
+ */
+public class ST10480553_PROG5121_POE {
+
+    public static void main(String[] args) {
+        Scanner myInput = new Scanner(System.in);
+        
+        //Declaration
+        String firstName;
+        String lastName;
+        String userName;
+        String phoneNum;
+        
+        //Ask user to enter name and surname
+        System.out.print("Enter first name: ");
+        firstName = myInput.nextLine();
+        
+        System.out.print("Enter last name: ");
+        lastName = myInput.nextLine();
+        
+        //
+    }
+}
