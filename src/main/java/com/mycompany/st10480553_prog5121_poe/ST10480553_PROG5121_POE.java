3335b332768ff5d3d4f3eef399f3fe86dc00ccde
dulcy <dulcy@MapulaDulcy.lan>
2026-09-16 23:23:27 +0200

    Initial commit: starting the project Added username validation using while loop

--- src/main/java/com/mycompany/st10480553_prog5121_poe/ST10480553_PROG5121_POE.java
+++ src/main/java/com/mycompany/st10480553_prog5121_poe/ST10480553_PROG5121_POE.java
@@ -14,7 +14,7 @@ public class ST10480553_PROG5121_POE {
 
     public static void main(String[] args) {
         Scanner myInput = new Scanner(System.in);
-        
+        Login login = new Login();
         //Declaration
         String firstName;
         String lastName;
@@ -28,6 +28,15 @@ public class ST10480553_PROG5121_POE {
         System.out.print("Enter last name: ");
         lastName = myInput.nextLine();
         
-        //
+        //Loop the username to check if it's valid or not
+        System.out.print("Enter Username(must contain '_' and max 5 char): ");
+        userName = myInput.nextLine();
+        
+        while(!login.checkUserName(userName)){
+            System.out.println("Username is not correctly formatted please ensure that the ussername contains an underscore and is more than five characters in length.");
+            System.out.print("Please re-enter username: ");
+            userName = myInput.nextLine();
+             }
+        
     }
 }
